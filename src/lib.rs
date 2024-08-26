e53d0df606f702198c1fbd7e60a2c3daa43ba54e
Pavlo Fedyna <fed.pavlo@gmail.com>
2024-08-26 16:10:20 +0200

    Add E2E tests. Add subscriptions endpoint

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::io::Error;
+use std::net::TcpListener;
+use actix_web::dev::Server;
+
+pub fn run(listener: TcpListener) -> Result<Server, Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
